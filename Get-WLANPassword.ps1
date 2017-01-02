194b9ce8096f12dba3eb9cd3f966865bae109dfb
BlackBoxCoder <blackboxcoder1337@googlemail.com>
2017-01-02 15:57:03 +0100

    Update Get-WLANPassword.ps1

--- Get-WLANPassword.ps1
+++ Get-WLANPassword.ps1
@@ -1,4 +1,13 @@
 Function Get-WLANPassword{
+    <#
+        .SYNOPSIS
+        This function displays all stored WLAN passwords on the client
+        .DESCRIPTION
+        This function gets the output of netsh command and will trim it to display all SSID`s and also the passwords.
+        If a profile has no password it will inform the user.
+        .EXAMPLE
+        Get-WLANPassword
+    #>
     $WLANProfileNames =@()
     #Get all the WLAN profile names
     $Output = netsh.exe wlan show profiles | Select-String -pattern " : "
@@ -9,8 +18,12 @@ Function Get-WLANPassword{
     $WLANProfileObjects =@()
     #Bind the WLAN profile names and also the password to a custom object
     Foreach($WLANProfileName in $WLANProfileNames){
-        #get the output for the specified profile name and trim the output to receive the password
-        $WLANProfilePassword = (((netsh.exe wlan show profiles name="$WLANProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
+        #get the output for the specified profile name and trim the output to receive the password if there is no password it will inform the user
+        try{
+            $WLANProfilePassword = (((netsh.exe wlan show profiles name="$WLANProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
+        }Catch{
+            Write-Warning "[!] The password is not stored in this profile"
+        }
         #Build the object and add this to an array
         $WLANProfileObject = New-Object PSCustomobject 
         $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfileName" -Value $WLANProfileName
