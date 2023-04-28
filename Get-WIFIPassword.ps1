bfa5d8e02f6ceffa9b400f1382bfbfea06c242f8
Christian Ritter <h.christian.ritter@gmail.com>
2023-04-28 20:36:00 +0200

    Update Get-WIFIPassword.ps1
    refactored the whole code, added support for German.

--- Get-WIFIPassword.ps1
+++ Get-WIFIPassword.ps1
@@ -1,37 +1,45 @@
-Function Get-WIFIPassword{
-    <#
-        .SYNOPSIS
-        This function displays all stored WIFI passwords on the client
-        .DESCRIPTION
-        This function gets the output of netsh command and will trim it to display all SSID`s and also the passwords.
-        If a profile has no password it will inform the user.
-        .EXAMPLE
-        Get-WLANPassword
-    #>
-    $WLANProfileNames =@()
-    #Get all the WLAN profile names
-    $Output = netsh.exe wlan show profiles | Select-String -pattern " : "
-    #Trim the output to receive only the name
-    Foreach($WLANProfileName in $Output){
-        $WLANProfileNames += (($WLANProfileName -split ":")[1]).Trim()
+<#
+    .SYNOPSIS
+    This function displays all stored WIFI passwords on the client
+    .DESCRIPTION
+    This function gets the output of netsh command and will trim it to display all SSID`s and also the passwords.
+    If a profile has no password it will inform the user.
+    .EXAMPLE
+    Get-WIFIPassword
+#>
+function Get-WIFIPassword {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+        #Regex-Pattern for GER/EN netsh.exe to search for the password
+        $Pattern = "Key Content|Schlüsselinhalt"
     }
-    $WLANProfileObjects =@()
-    #Bind the WLAN profile names and also the password to a custom object
-    Foreach($WLANProfileName in $WLANProfileNames){
-        #get the output for the specified profile name and trim the output to receive the password if there is no password it will inform the user
-        try{
-            $WLANProfilePassword = (((netsh.exe wlan show profiles name="$WLANProfileName" key=clear | select-string -Pattern "Key Content") -split ":")[1]).Trim()
-        }Catch{
-            Write-Warning "The password for $WLANProfileName is not stored in this profile, so it will be skipped"
-            break
+    
+    process {
+        $WifiProfileNames = foreach($ProfileName in $(netsh.exe wlan show profiles | Select-String -pattern " : ")){
+            (($ProfileName -split ":")[1]).Trim()
         }
-
-        #Build the object and add this to an array
-        $WLANProfileObject = New-Object PSCustomobject 
-        $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfileName" -Value $WLANProfileName
-        $WLANProfileObject | Add-Member -Type NoteProperty -Name "ProfilePassword" -Value $WLANProfilePassword
-        $WLANProfileObjects += $WLANProfileObject
-        Remove-Variable WLANProfileObject
+        $WifiProfileObjects = foreach($WifiProfileName in $WifiProfileNames){
+            [PSCustomObject]@{
+                ProfileName = $WifiProfileName
+                ProfilePassword = $ProfilePassword = $(
+                    try{
+                        (((netsh.exe wlan show profiles name="$WifiProfileName" key=clear | select-string -Pattern $Pattern) -split ":")[1]).Trim()
+                    }Catch{
+                        $null 
+                    }
+                )
+                Succeeded = $(
+                    ($Null -eq $ProfilePassword) ? ($false) : ($true)
+                )
+            }
+        }
     }
-    Write-Output $WLANProfileObjects
+    
+    end {
+        return $WifiProfileObjects
+    }
 }
